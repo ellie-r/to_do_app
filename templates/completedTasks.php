0dc930fc9555d3200af79cc2c3f53016122d8992
Ellie <erobinson98@btinternet.com>
2020-11-21 18:11:59 +0000

    styled checkboxes, added fonts and text color

--- templates/completedTasks.php
+++ templates/completedTasks.php
@@ -3,33 +3,31 @@
 <head>
     <meta charset="utf-8"/>
     <title>To Do</title>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Jost&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Gupter&family=Jost&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/d8f8b9f853.js" crossorigin="anonymous"></script>
     <link href="/css/style.css" type="text/css" rel="stylesheet">
 </head>
 <body>
-<h1>Let's get shit done!</h1>
 <h2>Completed Tasks</h2>
 <ul>
 <?php
 foreach($completedTasks as $task) {
     echo '<li>
-            <form method="post" action="/markAsUnComplete/' . $task['id'] . '">
+            <form class="" method="post" action="/markAsUnComplete/' . $task['id'] . '">
                  <input class="completeBox complete" type="submit" value =" ">
             </form>
             <p>'
-                . $task['name'] . '
+                . strtoupper($task['name']) . '
             </p>
             <form class="deleteTask" method="post" action="/deleteTask/' . $task['id'] . '">
-                 <input class="deleteBox" type="submit" value ="Delete">
+                 <input class="deleteBox button" type="submit" value ="x">
             </form>
           </li>';
 }
 ?>
 </ul>
 <footer>
-    <a href="https://iconscout.com/icons/check" target="_blank">Check Icon</a> by <a href="https://iconscout.com/contributors/feathericons">Feather Icons</a> on <a href="https://iconscout.com">Iconscout</a>
+    <a href="https://iconscout.com/icons/check" target="_blank">Check Icon</a> by <a href="https://iconscout.com/contributors/feathericons" target="_blank">Feather Icons</a>
 </footer>
 </body>
 </html>
--- templates/index.php
+++ templates/index.php
@@ -3,33 +3,42 @@
 <head>
     <meta charset="utf-8"/>
     <title>To Do</title>
-    <link href='//fonts.googleapis.com/css?family=Lato:300' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css2?family=Gupter&family=Jost&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/d8f8b9f853.js" crossorigin="anonymous"></script>
     <link href="/css/style.css" type="text/css" rel="stylesheet">
 </head>
 <body>
-    <h2>Add new Task</h2>
-    <div class="addTask">
-        <form  action="/addNewTask" method="post">
-            <label for="newTask">Add New Task</label>
-            <input type="text" id="newTask" name="newTask">
-            <input type="submit" value="Submit">
-        </form>
-    </div>
+    <section class="section">
+        <div class="addTask">
+            <form  action="/addNewTask" method="post">
+                <label for="newTask">Add New Task</label>
+                <input type="text" id="newTask" name="newTask">
+                <input class="button" type="submit" value="Submit">
+            </form>
+        </div>
+    </section>
+    <section class="section">
+        <h2>My Tasks</h2>
+        <ul class="tasks">
+        <?php
+            foreach($tasks as $task) {
+                echo '<li>
+                           <form method="post" action="/markAsComplete/' . $task['id'] . '">
+                                <input class="completeBox incomplete" type="submit" value =" ">
+                           </form><p>'
+                            . strtoupper($task['name']) .
+                      '</p></li>';
+            };
 
-    <h2>My Tasks</h2>
-    <ul class="tasks">
-    <?php
-        foreach($tasks as $task) {
-            echo '<li>
-                       <form method="post" action="/markAsComplete/' . $task['id'] . '">
-                            <input class="completeBox" type="submit" value =" ">
-                       </form><p>'
-                        . $task['name'] .
-                  '</p></li>';
-        };
-
-    ?>
-    </ul>
+        ?>
+        </ul>
+    </section>
+    <form method="get" action="/completeTasks">
+        <input class="button" type="submit" value ="View Complete Tasks">
+    </form>
+<footer>
+    <a href="https://iconscout.com/icons/circle" target="_blank">Circle Icon</a> by <a href="https://iconscout.com/contributors/daniel-bruce">Daniel Bruce</a> on <a href="https://iconscout.com">Iconscout</a>
+    <a href="https://iconscout.com/icons/check" target="_blank">Check Icon</a> by <a href="https://iconscout.com/contributors/feathericons" target="_blank">Feather Icons</a>
+</footer>
 </body>
 </html>
